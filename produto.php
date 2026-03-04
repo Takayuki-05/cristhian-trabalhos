c6f168e105e96c5aae3d34d433fe7ac92e301d03
Tici <ticiane.francisco@escola.pr.gov.br>
2026-03-04 11:24:12 -0300

    Add files via upload

---
+++ produto.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Produto</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container">
+			<div class="row">
+				<div class="col-sm-5">
+                    <!--local para foto do produto-->
+					Fotos do produto
+				</div>
+
+				<div class="col-sm-7">
+                      <!--local para informações(nome, valor, avaliação, pagamento)-->
+                    <h4> Pão francês com gegrgelim (kg) </h4>
+                    <hr>
+					Informção do produto
+					Preço parcelado(10x)
+                    Preço à vista (pix)
+					
+				</div>
+			 </div>
+
+                 <!--local para descrição completa do produto e demais caracteristicas-->
+             <div class="row">
+                <div class="col-sm-12">
+
+                </div>
+
+            </div>
+        </div>
+   </body>
+  </html>
