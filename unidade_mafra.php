bb120c48f706d26be29db027442fc0ed3f411025
Tici <ticiane.francisco@escola.pr.gov.br>
2026-03-18 11:21:29 -0300

    Add files via upload

---
+++ unidade_mafra.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>unidade mafra/SC</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </head>
+    <body>
+		<br>
+        <?php
+            include ("quemsomos.php")
+        ?>
+        <div class="container">
+            <br><hr><br>
+            <center>
+                <b></b>
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1005.3078055492834!2d-49.80499859724421!3d-26.11503043879195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjbCsDA2JzUzLjEiUyA0OcKwNDgnMTkuNCJX!5e0!3m2!1spt-BR!2sbr!4v1773843010009!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </center>
+
+
+        </div>
+    </body>
+ </html>
