78d07ec043863fa687677a118e7382c85f619959
Tici <ticiane.francisco@escola.pr.gov.br>
2026-03-23 12:12:34 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Padaria da Titako</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <!--div para imagem da panificadora-->
+        <div class="row">
+            <div class="col-md-12">
+                <img src="https://lh3.googleusercontent.com/gg-dl/AOI_d__Jl40KOxLP6J1G9Ap-5lv_VSUAUqSwhqUQGn8CC_FkbUSDzUefOs2sl2x8d5kkDcE7oKFf0CrGFU43otujXm9iBo9eqB9E__Az7RUyXUx5WCKRlfYQw3vgJL6y6Lbj5tsDA0UG4FXzGQUS2Cv_dAKrEJJfNEEiy5MksD9ZvLHn8XAG=s1600-rj" class="img-fluid" style="border-radius: 5%;">
+            </div>
+            <div class="col-md-7">
+            
+            
+        </div>
+    </div>
+    <!--Div para o menu em hamburguer móbile-->
+    <div class="row"> 
+    </div>
+    </body>
+</html>
--- quemsomos.php
+++ quemsomos.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+		<title>Unidade Mafra/SC</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </head>
+    <body>
+		<br>
+
+        <!DOCTYPE html>
+<html>
+    <head>
 		<title>Quem somos</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -19,7 +30,7 @@
 						</svg>&nbsp;
 						Padaria da Titako
 					</h3>
-                    <h5>Sempre com a Tradição Italiana</h5>
+                    <h5>Com uma grande tradição Italiana</h5>
 				</div>
 			</div>
 
@@ -30,7 +41,7 @@
                 </div>
                 <div class="col-sm-7">
                     <p aling="justify">
-                        Há mais de seis décadas, a Padaria do Nono Careca traz o verdadeiro sabor da Itália para a região central e nobre de Mafra. 
+                        Há mais de seis décadas, a <b>Padaria do Nono Careca</b> traz o verdadeiro sabor da Itália para a região central e nobre de Mafra. 
                         O que começou no forno a lenha do nosso fundador hoje se estende por mais de 3 filiais, espalhando nosso carinho por cidades 
                         de Santa Catarina e Paraná.
                     </p>
@@ -44,12 +55,77 @@
                     </p>
 
                     <center>
-                        <span class="badge text-bg-info">&nbsp;&nbsp;Rio Negro/PR&nbsp;&nbsp;</span>&nbsp;
-                        <span class="badge text-bg-info">&nbsp;&nbsp;mafra/SC&nbsp;&nbsp;</span>&nbsp;
-                        <span class="badge text-bg-info">&nbsp;&nbsp;Guaratuba/PR&nbsp;&nbsp;</span>&nbsp;
-                        <span class="badge text-bg-info">&nbsp;&nbsp;Lapa/PR&nbsp;&nbsp;</span>&nbsp;
+
+                    <!-- Botão do WhatsApp para contato -->
+                    <a href="https://api.whatsapp.com/send?phopne=47988770123&text=Gostaria de uma informação.">
+                        <button class="btn btn-success">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+                            </svg>&nbsp;
+                            Fale conosco
+                        </button>
+                    </a>
+
+                    <br><br>
+
+                        <a href="unidade_mafra.php" style="text-decoration: none;">
+                            <span class="badge text-bg-info">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>
+                                &nbsp;&nbsp;Mafra/SC&nbsp;&nbsp;
+                            </span>
+                        </a>
+                        &nbsp;
+
+                        <a href="unidade_joinville.php" style="text-decoration: none;">
+                            <span class="badge text-bg-info">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>                        
+                                &nbsp;&nbsp;Joinville/SC&nbsp;&nbsp;
+                            </span>
+                        </a>
+                        &nbsp;
+                        
+                        <span class="badge text-bg-info">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>
+                            &nbsp;&nbsp;Balneário Camboriu/SC&nbsp;&nbsp;
+                        </span>&nbsp;
+                        
+                        <span class="badge text-bg-info">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>
+                            &nbsp;&nbsp;Curitiba/PR&nbsp;&nbsp;
+                        </span>&nbsp;
                     </center>
                 </div>
 
             </div>
 
+
+            
+
+
+
+
+
+
+        <div class="container">
+            <br><hr><br>
+            <center>
+                <b>R. Felipe Schmidt, 509 - Centro I Baixada, Mafra - SC, 89300-178</b><br><br>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d895.6216194426904!2d-49.805757673331904!3d-26.115676718625004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dd9319b744848d%3A0x68934be91450bbe0!2sR.%20Felipe%20Schmidt%2C%20509%20-%20Centro%20I%20Baixada%2C%20Mafra%20-%20SC%2C%2089300-178!5e0!3m2!1spt-BR!2sbr!4v1773842197065!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </center>
+
+        </div>
+    </body>
+</html>
+    
