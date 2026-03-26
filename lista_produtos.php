e194a015556d841cbb20b71630f47870e27a5bf8
Tici <ticiane.francisco@escola.pr.gov.br>
2026-03-26 07:36:42 -0300

    Add files via upload

--- lista_produtos.php
+++ lista_produtos.php
@@ -1,86 +1,76 @@
 <!DOCTYPE html>
 <html>
     <head>
-      <title>Produto</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+		<title>Lista de produtos</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </head>
     <body>
 		<br>
         <div class="container">
-            <br>
-            <div class="row">
-                <!--div para o cabeçario da página-->
-                <div class ="col-sm-12">
-                    <h3>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop-window" viewBox="0 0 16 16">
-                            <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5m2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5"/>
-                        </svg>&nbsp; 
-                        Lista de produtos
-                    </h3>
-                </div>
-            </div>
 			<br>
-            <div class="container">
-                <br>
-            <div class="row">
-            
-                <!--LINHA DO CABEÇALHO DA TABELA (DIVI)-->
+			<div class="row">
+				<!-- Div para o cabeçalho da página -->
+				<div class="col-sm-12">
+					<h3>
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-shop-window" viewBox="0 0 16 16">
+							<path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5m2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5"/>
+						</svg>&nbsp;
+						Lista de produtos
+					</h3>
+				</div>
+			</div>
 
-                <!--div da foto do produto-->
-                <div class="col-sm-1" style="background-color:pink">
-                   <b>Fotos </b>
-                </div>
-
-               <!--div para apresentar o nome do produto-->
-                <div class="col-sm-7" style="background-color:pink">
-                    <b>descrição</b>
-               </div>
-
-               <!--div para apresentar o preço do produto-->
-                <div class="col-sm-2" style="background-color:pink">
-                    <b>preço(R$)</b>
-               </div>
-
-               <!--div para ações referente ao produto-->
-                <div class="col-sm-2" style="background-color:pink">
-                     <b>ações</b>
-               </div>
-                </div>
-
-<!------------------------------------------------------------------------------------------------------------------------------------------------->
-
-             <!--LINHA(S) DO(S) REGISTROS (PRODUTOS)-->
-             <br>
-             <div class="row">
-                <!--LINHA DO CABEÇALHO DA TABELA (DIVI)-->
-                <!--DIV DA FOTO DO PRODUTO-->
-
-             <div class="col-sm-2">
-                <img src="https://paesnovorio.com.br/web/image/product.template/466/image_1024?unique=e45346a" class="img-fluid" style="border-radius: 5px;">"
-            </div>
-
-            <!---DIV PARA APRESENTAR O NOME DO PRODUTO-->
-             <div class="col-sm-6">
-                pão francês com gergelim (kg)
-            </div>
-
-             <!---DIV PARA APRESENTAR O PREÇO DO PRODUTO-->
-             <div class="col-sm-2">
-                R$ 14,99
-            </div>
-
-             <!---DIV PARA AÇÕES REFERENTES AO PRODUTO -->
-            <div class="col-sm-2">
-                <a href="paofrancescomgergelim.php">
-                    <button class="btn btn-success">
-                        Ver mais
-                    </button>
-                </a>    
-            
-            </div>
-        </div>
-       </div>
-    </body>
-<html>
+			<br>
+			<div class="row">
+				<!-- LINHA DO CABEÇALHO DA TABELA (DIV) -->
+				<!-- Div da foto do produto -->
+				<div class="col-sm-2" style="background-color: #cccccc;">
+					<b>Foto</b>
+				</div>
+			
+				<!-- Div para apresentar o nome do produto -->
+				<div class="col-sm-6" style="background-color: #cccccc;">
+					<b>Descricao</b>
+				</div>
+				
+				<!-- Div para apresentar o preço do produto -->
+				<div class="col-sm-2" style="background-color: #cccccc;">
+					<b>Preço (R$)</b>
+				</div>
+				
+				<!-- Div para ações referente ao produto -->
+				<div class="col-sm-2" style="background-color: #cccccc;">
+					<b>Ações</b>
+				</div>
+			</div>
+			
+			<! ------------------------------------------------------- ->
+			<br>
+			<!-- LINHA(S) DO(S) REGISTROS (PRODUTOS) -->
+			<div class="row">
+				<div class="col-sm-2">
+					<img src="img/image_1024.jpg" width="100">
+				</div>
+			
+				<div class="col-sm-6"> 
+					Pão Francês com gergelim (kg)
+				</div>
+				
+				<div class="col-sm-2">
+					R$ 14,99
+				</div>
+				
+				<div class="col-sm-2">
+					<a href="paofrancesgergelim.php">
+						<button class="btn btn-success">Ver mais</button> 
+					</a>
+				</div>
+			</div>
+			
+			
+			
+		</div>
+	</body>
+</html>
