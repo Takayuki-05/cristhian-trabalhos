50a74bc2763b087911c2cdb2cfc0e8d3cd3e7edc
Tici <ticiane.francisco@escola.pr.gov.br>
2026-04-06 12:07:31 -0300

    Add files via upload

--- teste.php
+++ teste.php
@@ -1,17 +1,34 @@
-<?php
-    //variáveis do PHP
-    $nome               =    "Megumi"
-    $sobrenome          =    "Fushiguro"
-    $cidade             =    "Tokyo"
-    $país               =    "Japão"
-    $idade              =     17;
-    $salario            =     16.000;
-    $salario_liquido    =     15.800;
-    $desconto_salario   =     200.00;
-    $empregador         =     "Escola Jujutsu de Tokyo"
 
-    echo "BEM VINDO <b>$nome $sobrenome</b>, morador da cidade de $cidade/$país.<br>";
-    echo "Com $idade anos de idade, empregado no(a) $empregador, recebe um sálario bruto de R$ $salario.<br>";
-    echo "e salário líquido de R$ $salario_liquido mensal."
+<?php
+    //Variáveis do PHP
+    $nome                =   "Tamsy";
+    $sobrenome           =   "Caines";
+    $cidade              =   "Mafra";
+    $estado              =   "Santa Catarina";
+    $idade               =    23;
+    $salario             =    6700;
+    $desconto_salario    =    325.75;
+    $salario_liquido     =    $salario - $desconto_salario; //calcula o salário líquido
+    $empregador          =   "Cleaners Ltda";
+
+    echo "Bem Vindo <b>$nome $sobrenome</b>!<br>";
+    echo "com $idade anos de idade, empregado no(a) <b>$empregador</b>, recebe um salário bruto de  R$ " .number_format($salario, 2, ',', '.') . " e salário líquido de R$ " .number_format($salario_liquido, 2, ',', '.') . " mensal.";
+    echo " e salário líquido de R$ " .number_format($salario_liquido, 2, ',', '.') . " mensal.";
+
+    echo "<hr>";
+
+    if($idade <= 12){
+        echo "O(A) usuário(a) $nome é crianca!";
+        header("Location: https://www.disney.com");
+    } elseif($idade <= 17){
+        echo "O(A) usuário(a) $nome é adolescente!";
+        header("Location: https://www.instagram.com");
+    } elseif($idade <= 59){
+        echo "O(A) usuário(a) $nome é adulto!";
+        header("Location: https://www.cnn.com");
+    } else {
+        echo "O(A) usuário(a) $nome é idoso!";
+        header("Location: https://www.essentialnutrition.com.br/produtos/omega-3");
+    }
 
 ?>
