5103d238d217934c08819495e179291eaaca9d75
Tici <ticiane.francisco@escola.pr.gov.br>
2026-04-06 07:52:32 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php
+    //variáveis do PHP
+    $nome               =    "Megumi"
+    $sobrenome          =    "Fushiguro"
+    $cidade             =    "Tokyo"
+    $país               =    "Japão"
+    $idade              =     17;
+    $salario            =     16.000;
+    $salario_liquido    =     15.800;
+    $desconto_salario   =     200.00;
+    $empregador         =     "Escola Jujutsu de Tokyo"
+
+    echo "BEM VINDO <b>$nome $sobrenome</b>, morador da cidade de $cidade/$país.<br>";
+    echo "Com $idade anos de idade, empregado no(a) $empregador, recebe um sálario bruto de R$ $salario.<br>";
+    echo "e salário líquido de R$ $salario_liquido mensal."
+
+?>
