ddb0a93dd5a7ada2c5eee57f018ff6cf2c3c5dd7
Tici <ticiane.francisco@escola.pr.gov.br>
2026-04-22 11:26:04 -0300

    Add files via upload

---
+++ novo_pet.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novo pet</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+<body>
+   <div class="container">
+      <div class="row">
+        <div class="div-sm-12">
+            <h3>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="green" class="bi bi-bluesky" viewBox="0 0 16 16">
+                <path d="M3.468 1.948C5.303 3.325 7.276 6.118 8 7.616c.725-1.498 2.698-4.29 4.532-5.668C13.855.955 16 .186 16 2.632c0 .489-.28 4.105-.444 4.692-.572 2.04-2.653 2.561-4.504 2.246 3.236.551 4.06 2.375 2.281 4.2-3.376 3.464-4.852-.87-5.23-1.98-.07-.204-.103-.3-.103-.218 0-.081-.033.014-.102.218-.379 1.11-1.855 5.444-5.231 1.98-1.778-1.825-.955-3.65 2.28-4.2-1.85.315-3.932-.205-4.503-2.246C.28 6.737 0 3.12 0 2.632 0 .186 2.145.955 3.468 1.948"/>
+                </svg>&nbsp;
+                Novo pet
+            </h3>
+ 
+        </div>
+      </div>
+
+    tipo: cachorro, gato, aves
+    raça: srd, rottweiler, etc... 
+    pelo: curto, grande, longo sem pelo
+    cor: preto branco, cinza
+    porte: pequeno, grande
+    peso:_______ kg 
+    sexo:f, m 
+
+   </div> 
+</body>
+</html>
